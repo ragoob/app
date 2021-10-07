3ff40ec540c1474367e9baf4f40144907b8e1927
Mohamed Ragab <mohamed.ragab.ext@aptar.com>
2021-10-07 23:18:25 +0200

    sort resources and add scale de-scale deployments and adding users managment

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,24 @@
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: 'API_URL',
+  ws: 'SOCKET_URL',
+  api: {
+    k8s: '/connections',
+    auth: '/auth',
+    clusters: '/clusters',
+    nameSpaces: '/namespaces',
+    nodes: '/nodes',
+    deployments: '/deployments',
+    services: '/services',
+    pods: '/pods',
+    ingresses: '/ingress',
+    secrets: '/secrets',
+    events: '/events',
+    users: '/users',
+  },
+  sockets: {
+      health: '/healthaknowlegment',
+      monitoring: "/monitoring",
+      outbound: "/outbound"
+  }
 };
